6ba0f81b72fb154a45b3ed4d97be4ce715091443
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-14 11:38:48 +0100

    update

--- src/notesReducer.js
+++ src/notesReducer.js
@@ -1,36 +1,51 @@
-import { ADD_NOTE, setNotes, SET_NOTES } from "./actions"
+import { ADD_NOTE, setNotes, SET_NOTES } from "./actions";
+import axios from "axios";
 
 const initialState = {
-  notes: []
-}
+  notes: [],
+};
 
 export const notesReducer = (state = initialState, action) => {
-  switch(action.type){
+  switch (action.type) {
     case ADD_NOTE: {
-      return {...state, notes: [...state.notes, action.payload]}
+      return { ...state, notes: [...state.notes, action.payload] };
     }
     case SET_NOTES: {
-      return {...state, notes: action.payload}
+      return { ...state, notes: action.payload };
     }
     default:
-      return state
+      return state;
   }
-}
+};
 
 export const saveNotes = () => async (dispatch, getState) => {
   const notes = getState().notes;
   await fetch("http://localhost:4000/notes", {
     method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-type': 'application/json'
+      Accept: "application/json",
+      "Content-type": "application/json",
     },
-    body: JSON.stringify(notes)
-  })
-  alert("Success")
-} 
+    body: JSON.stringify(notes),
+  });
+  alert("Success");
+};
 
 export const loadNotes = () => async (dispatch, getState) => {
-  const notes = await fetch("http://localhost:8080/index.php/wp-json/trabajadores/v1/todos").then(res => res.json())
-  dispatch(setNotes(notes))
-} 
+  const url = "http://localhost:8080/index.php/wp-json/trabajadores/v1/todos";
+
+  const config = {
+    url: url,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+    },
+  };
+
+  const notes = await axios(config).then((res) => res.data);
+
+  console.log(notes);
+
+  dispatch(setNotes(notes));
+};
